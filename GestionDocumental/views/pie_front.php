aec9fa94b5ac23c7cd7a92bf646fa8e7645b4213
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-10-27 19:28:08 +0000

    primera version

---
+++ GestionDocumental/views/pie_front.php
@@ -0,0 +1,92 @@
+
+<div class="foot">
+<table width="90%" align="center" >
+	<tr>
+    	<td height="30">
+        </td>
+    </tr>
+	<tr>
+    	<td valign="top"  width="20%">
+        	<table width="100%">
+            	<tr>
+                	<td>
+                    	<label class="tit_foot">Cont&aacute;ctenos</label>
+                    </td>
+                </tr>
+                <tr>
+                	<td valign="top">
+                    <table>
+                    	<tr>
+                        	<td> <img src="images/mail.png"></td>
+                        	<td>
+			                 <font class="txt_foot">support@seguridad-ip.com</font>
+                    		</td>
+                    	</tr>
+                        <tr>
+                        	<td><img src="images/adress.png"></td>
+                        	<td>
+							<font class="txt_foot">Santiago de Chile, Chile</font>
+                            </td>
+                        </tr>
+                    </table>
+                    </td>
+                </tr>
+            </table>
+		</td>
+        <td  valign="top"  width="40%">
+        	<table  width="100%">
+            	<tr>
+                	<td>
+                    	<label class="tit_foot">Acerca de nosotros</label>
+                    </td>
+                </tr>
+                <tr>
+                	<td>
+                    <font class="txt_foot">
+                    Seguridad-Ip podr&iacute;a ser la herramienta de seguridad que ha estado esperando.
+                    </font>
+                    </td>
+                </tr>
+            </table>
+        </td>
+        <td  valign="top" width="40%">
+        	<table  width="100%">
+            	<tr>
+                	<td>
+                    	<label class="tit_foot">Novedades</label>
+                    </td>
+                </tr>
+                <tr>
+                	<td>
+                     <font class="txt_foot">
+                    Mant&eacute;ngase al d&iacute;a con nuestras &uacute;ltimas noticias y comunicados de productos mediante la firma de nuestro bolet&iacute;n de noticias.
+                    
+                    </font>
+                    </td>
+                </tr>
+                <tr>
+                	<td>
+                     <font class="txt_foot">
+                    
+                    
+                     <div class="content_op"> 
+                     <input type="email" name="mail_new" id="mail_new" placeholder="Email" size="30">
+                    <button class="selected_op" onClick="smail()">OK</button>
+                    </div>
+                    </font>
+                    <span id="mensaje_news" class="mensaje_news">Loooo</span>
+                    </td>
+                </tr>
+            </table>
+        </td>
+	</tr>
+</table>
+<hr class="sep_foot">
+<table width="90%" style="position:relative; margin-top:15px;"  align="center">
+	<tr>
+    	<td align="left">
+        <a href="#" class="txt_foot">Seguridad-Ip</a><font class="txt_foot"> &Iota; Copyright <?=date("Y")?> &copy; All Rights Reserved.</font>
+        </td>
+    </tr>
+</table>
+</div>
