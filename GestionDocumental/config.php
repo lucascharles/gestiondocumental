0f5488550112c18afc167e2d37e9af3ce8a7963a
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-10-27 19:06:45 +0000

    se setena nombre de proyecto variables de configruacion

--- GestionDocumental/config.php
+++ GestionDocumental/config.php
@@ -4,45 +4,16 @@ $config = Config::singleton();
 $config->set('controllersFolder', 'controllers/');
 $config->set('modelsFolder', 'models/');
 $config->set('viewsFolder', 'views/');
-$config->set('nombre_sistema','SEGURIDAD IP');
+$config->set('nombre_sistema','GESTION DOCUMENTAL');
 $config->set('nombre_empresa','BCH-SOLUCIONES S.A.');
-//$config->set('nombre_empresa','BCH-SOLUCIONES');
-
 
 // CONFIGURACION MYSQL
-
-// maquina local
-
 $config->set('dbhost', 'localhost');
-$config->set('dbname', 'seguridadip');
-//$config->set('dbname', 'test');
+$config->set('dbname', 'gestion_documental');
 $config->set('dbuser', 'root');
-//$config->set('dbuser', 'bchsoluc_dyv');
 $config->set('dbpass', 'root');
-//$config->set('dbpass', 'bchsoluc_dyv');
-
-$config->set('modulo_panelcontrol', '1');
-$config->set('modulo_informes', '2');
-$config->set('modulo_dispositivo', '3');
-$config->set('modulo_usuarios', '4');
+$config->set('carpeta_archivos_general', 'docvarios/');
 
 $_SESSION["config_obj"] = $config;
-
-
-
-$include_url = $_SERVER[DOCUMENT_ROOT]."/seguridadip";
-//$include_url = $_SERVER[DOCUMENT_ROOT]."/demos/seguridadip";
-
-
-/*
-// server
-
-$config->set('dbhost', 'localhost');
-$config->set('dbname', 'bchsoluc_sistemadv');
-$config->set('dbuser', 'bchsoluc_root');
-$config->set('dbpass', 'bchroot123');
-
-$include_url = $_SERVER[DOCUMENT_ROOT]."/demo/gcontratista";
-*/
-
+$include_url = $_SERVER[DOCUMENT_ROOT]."/gestiondocumental";
 ?>
