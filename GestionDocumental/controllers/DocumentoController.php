cf5050e6c663574c54e340568ff11d60564cdc2d
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-10-27 19:12:36 +0000

    primera version svn

---
+++ GestionDocumental/controllers/DocumentoController.php
@@ -0,0 +1,59 @@
+<?php
+class DocumentoController extends ControllerBase
+{
+    public function carga($param)
+    {
+		require 'models/ContratistaModel.php';
+		require 'models/DocumentoModel.php';
+		$dato = new ContratistaModel();
+		$doc = new DocumentoModel();
+		$param["id"] = $_SESSION["userid"];
+		$data["dato"] = $dato->getContratista($param);
+		$data["idsql_tip_doc"] = $doc->getListaTipoDocumento();
+		if($param["id_faena"] > 0) $_SESSION["f_id_faena"] = $param["id_faena"]; else $param["id_faena"] = $_SESSION["f_id_faena"];
+		if($param["id_tipo_documento"] > 0)	$_SESSION["f_id_tipo_documento"] = $param["id_tipo_documento"]; else $param["id_tipo_documento"] = $_SESSION["f_id_tipo_documento"];
+		$data["idsql_faena"] = $dato->getFaenasContratista($param);
+
+		if($param["id_faena"] > 0 && $param["id_tipo_documento"] > 0)
+		{
+			$param["id_contratista"] = $_SESSION["userid"];
+			$data["idsql_doc"] = $doc->getDocumentos($param);
+		}
+		$data['nom_sistema'] = $param["nombre_sistema"];
+		$data['nom_empresa'] = $param["nombre_empresa"];
+		$data['arrayscriptJs'] = array("validacampos.js","documento.js","home.js","cabecera.js");
+		$data['arrayscriptCss'] = array("home.css","sticky-navigation.css");
+		
+		$this->view->show("person/carga_doc_empresa.php", $data);
+    }
+	
+	public function grabasubir_archivo($param)
+	{
+		require 'models/ContratistaModel.php';
+		require 'models/DocumentoModel.php';
+		$dato = new ContratistaModel();
+		$doc = new DocumentoModel();
+		
+		$_SESSION["f_id_faena"] = $param["id_faena"];
+		$param["id_tipo_documento"] = $_SESSION["f_id_tipo_documento"];
+		$param["id_contratista"] = $_SESSION["userid"];
+		
+		$doc->uploadDocumento($param);
+		
+		$param["id"] = $_SESSION["userid"];
+		$data["dato"] = $dato->getContratista($param);
+		$data["idsql_tip_doc"] = $doc->getListaTipoDocumento();
+		if($param["id_faena"] > 0) $_SESSION["f_id_faena"] = $param["id_faena"]; else $param["id_faena"] = $_SESSION["f_id_faena"];
+		if($param["id_tipo_documento"] > 0)	$_SESSION["f_id_tipo_documento"] = $param["id_tipo_documento"]; else $param["id_tipo_documento"] = $_SESSION["f_id_tipo_documento"];
+		$data["idsql_faena"] = $dato->getFaenasContratista($param);
+		
+		$data["idsql_doc"] = $doc->getDocumentos($param);
+		$data['nom_sistema'] = $param["nombre_sistema"];
+		$data['controller'] = $param["controlador"];
+		$data['arrayscriptJs'] = array("validacampos.js","documento.js","home.js","cabecera.js");
+		$data['arrayscriptCss'] = array("home.css","sticky-navigation.css");
+		
+		$this->view->show("person/carga_doc_empresa.php", $data);
+	}
+}
+?>
