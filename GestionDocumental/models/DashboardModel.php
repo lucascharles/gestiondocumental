9ec13ed6b7d29303295660dbd8cbbbe20a157ac5
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-10-28 03:21:59 +0000

    ajuste funcion getListaConstructoras

--- GestionDocumental/models/DashboardModel.php
+++ GestionDocumental/models/DashboardModel.php
@@ -6,37 +6,25 @@ class DashboardModel extends ModelBase
 	{
 		include("config.php");
 		
-		$select = " c.consIdConstructora consIdConstructora, c.consNombreFantasia consNombreFantasia, c.consRazonSocial consRazonSocial, c.consRut consRut ";
-		$from = " constructora c ";
-		$where = " c.activo = 'S' ";
+		$sql = " SELECT c.consIdConstructora consIdConstructora, c.consNombreFantasia consNombreFantasia, c.consRazonSocial consRazonSocial, c.consRut consRut ";
+		$sql .= " FROM constructora c ";
+		$sql .= " WHERE c.activo = 'S' ";
 		
 		if(trim($array["consRazonSocial"]) <> "")
 		{
-			$where .= " and c.consRazonSocial LIKE '".trim($array["consRazonSocial"])."%'";
+			$sql .= " and c.consRazonSocial LIKE '".trim($array["consRazonSocial"])."%'";
 		}
 
 		if(trim($array["consRut"]) <> "")
 		{
-			$where .= " and c.consRut LIKE '".trim($array["consRut"])."%'";
+			$sql .= " and c.consRut LIKE '".trim($array["consRut"])."%'";
 		}
 		
-		$where .= " ORDER BY c.consRazonSocial ";
-		
-		$sqlpersonal = new SqlPersonalizado($config->get('dbhost'), $config->get('dbuser'), $config->get('dbpass') );
-		$sqlpersonal->set_select($select); 
-	  	$sqlpersonal->set_from($from);
-		$sqlpersonal->set_where($where);
-		if(!($array["all_rows"] == "S"))
-		{
-			$sqlpersonal->set_limit(($array["inicio"]*40),($array["inicio"]*40)+40); // PARA MYSQL
-		}
+		$sql .= " ORDER BY c.consRazonSocial ";
 	
-    	$sqlpersonal->load();
-		$cant = $sqlpersonal->get_cant_registros();
 		
-		$result = array();
-		$result[] = $sqlpersonal;
-		$result[] = $cant;
+		$result = consulta($sql);
+		
     	return $result;	
 	}
 
