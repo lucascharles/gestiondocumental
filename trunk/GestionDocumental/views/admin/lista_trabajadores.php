d275ce92ad449a3e7230a5480d502f7616c2349f
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-10-28 03:29:41 +0000

    ajuste uso de coleccion de datos

--- trunk/GestionDocumental/views/admin/lista_trabajadores.php
+++ trunk/GestionDocumental/views/admin/lista_trabajadores.php
@@ -2,15 +2,6 @@
 include("views/cabecera_listado.php"); 
 ?>
 <table width="100%" cellpadding="0" cellspacing="0" align="center" border="0" bgcolor="#FFFFFF" id="listado">
-	<?
-    	$coleccion = $result[0];
-		$cant = $result[1];
-		if($cant > 40)
-		{
-			include ('views/paginador.php');
-		}
-	?>
-	
 	<tr>
     	<th width="" align="center" height="25"></th>
 		<th align="left" width="">ID TRABAJADOR</th>
@@ -24,30 +15,28 @@ include("views/cabecera_listado.php");
     </tr>
 	<?php
 	
-	for($j=0; $j<$coleccion->get_count(); $j++) 
-	{
-		$datoTmp = &$coleccion->items[$j];
-			
+	while($rs = mysql_fetch_array($result))
+	{	
 	?>
 	
-	<tr bgcolor="#FFFFFF" id="fila_<?php echo ($datoTmp->get_data("id")) ?>">
+	<tr bgcolor="#FFFFFF" id="fila_<?php echo ($rs["id"]) ?>">
     	<td height="" width="5%"></td>
-        <td align="left" width="10%"><?php echo ($datoTmp->get_data("trbIdTrabajador")) ?></td>
-		<td align="left" width="15%"><?php echo (utf8_decode($datoTmp->get_data("trbNombre"))) ?></td>
-        <td align="left" width="15%"><?php echo (utf8_decode($datoTmp->get_data("trbApPaterno"))) ?></td>
-      <!--  <td align="left" width="10%"><?=($datoTmp->get_data("fec_alta")=="0000-00-00") ? "" : formatoFecha($datoTmp->get_data("fec_alta"),"yyyy-mm-dd","dd/mm/yyyy"); ?></td> -->
-		<td align="left" width="10%"><?php echo (utf8_decode($datoTmp->get_data("trbApPaterno"))) ?></td>
-		<td align="left" width="10%"><?php echo (utf8_decode($datoTmp->get_data("trbApPaterno"))) ?></td>
-		<td align="left" width="10%"><?php echo (utf8_decode($datoTmp->get_data("trbApPaterno"))) ?></td>    
+        <td align="left" width="10%"><?php echo ($rs["trbIdTrabajador"]) ?></td>
+		<td align="left" width="15%"><?php echo (utf8_decode($rs["trbNombre"])) ?></td>
+        <td align="left" width="15%"><?php echo (utf8_decode($rs["trbApPaterno"])) ?></td>
+      <!--  <td align="left" width="10%"><?=($rs["fec_alta"]=="0000-00-00") ? "" : formatoFecha($rs["fec_alta"],"yyyy-mm-dd","dd/mm/yyyy"); ?></td> -->
+		<td align="left" width="10%"><?php echo (utf8_decode($rs["trbApPaterno"])) ?></td>
+		<td align="left" width="10%"><?php echo (utf8_decode($rs["trbApPaterno"])) ?></td>
+		<td align="left" width="10%"><?php echo (utf8_decode($rs["trbApPaterno"])) ?></td>    
 		<td align="left" width="15%">
-      <!--  <img src="images/permisos.png" title="Permisos" class="oplistado" onclick="window.parent.configurarRegistro('<? echo($controller) ?>',<?php echo ($datoTmp->get_data("id")) ?>)" /> -->
-        <img src="images/editar.gif" title="Editar" class="oplistado" onclick="window.parent.editarRegistro('<? echo($controller) ?>',<?php echo ($datoTmp->get_data("trbIdTrabajador")) ?>)" />
-        <img src="images/borrar.gif" title="Eliminar" class="oplistado" onclick="window.parent.abrirVentanaConfirmacion(<?php echo ($datoTmp->get_data("trbIdTrabajador")) ?>)" />
-        <img src="images/permisos.png" title="Documentos" class="oplistado" onclick="window.parent.documentosTrabajador('<? echo($controller) ?>',<?php echo ($datoTmp->get_data("trbIdTrabajador")) ?>)" />
+      <!--  <img src="images/permisos.png" title="Permisos" class="oplistado" onclick="window.parent.configurarRegistro('<? echo($controller) ?>',<?php echo ($rs["id"]) ?>)" /> -->
+        <img src="images/editar.gif" title="Editar" class="oplistado" onclick="window.parent.editarRegistro('<? echo($controller) ?>',<?php echo ($rs["trbIdTrabajador"]) ?>)" />
+        <img src="images/borrar.gif" title="Eliminar" class="oplistado" onclick="window.parent.abrirVentanaConfirmacion(<?php echo ($rs["trbIdTrabajador"]) ?>)" />
+        <img src="images/permisos.png" title="Documentos" class="oplistado" onclick="window.parent.documentosTrabajador('<? echo($controller) ?>',<?php echo ($rs["trbIdTrabajador"]) ?>)" />
         </td>
 	</tr>
-    <tr bgcolor="#FFFFFF" id="fila_sep_<?php echo ($datoTmp->get_data("id")) ?>">
-    	<td colspan="6" style="border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC; "></td>
+    <tr bgcolor="#FFFFFF" id="fila_sep_<?php echo ($rs["id"]) ?>">
+    	<td colspan="9" style="border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC; "></td>
 	</tr>
         
 	<?php
