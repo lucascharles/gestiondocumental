d472d017044cbbd9a03182b034bf46d415afc996
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-10-30 01:01:48 +0000

    ajuste visual y uso de coleccion de datos sql

--- trunk/GestionDocumental/views/admin/lista_faenas.php
+++ trunk/GestionDocumental/views/admin/lista_faenas.php
@@ -2,20 +2,12 @@
 include("views/cabecera_listado.php"); 
 ?>
 <table width="100%" cellpadding="0" cellspacing="0" align="center" border="0" bgcolor="#FFFFFF" id="listado">
-	<?
-    	$coleccion = $result[0];
-		$cant = $result[1];
-		if($cant > 40)
-		{
-			include ('views/paginador.php');
-		}
-	?>
 	
 	<tr>
     	<th width="" align="center" height="25"></th>
-		<th align="left" width="">ID FAENA</th>
-        <th align="left" width="">NOMBRE FAENA</th>
-        <th align="left" width="">ESTADO FAENA</th>
+		<th align="left" width="">ID</th>
+        <th align="left" width="">NOMBRE</th>
+        <th align="left" width="">ESTADO</th>
 		<th align="left" width="">FECHA INICIO</th>
 		<th align="left" width="">FECHA FIN</th>
 		<th align="left" width="">RESPONSABLE</th>
@@ -24,29 +16,27 @@ include("views/cabecera_listado.php");
     </tr>
 	<?php
 	
-	for($j=0; $j<$coleccion->get_count(); $j++) 
-	{
-		$datoTmp = &$coleccion->items[$j];
-			
+	while($rs=mysql_fetch_array($result))
+	{		
 	?>
-	<tr bgcolor="#FFFFFF" id="fila_<?php echo ($datoTmp->get_data("id")) ?>">
+	<tr bgcolor="#FFFFFF" id="fila_<?php echo ($rs["id"]) ?>">
     	<td height="" width="5%"></td>
-		<td align="left" width="5%"><?php echo (utf8_decode($datoTmp->get_data("id"))) ?></td>
-		<td align="left" width="15%"><?php echo (utf8_decode($datoTmp->get_data("faeNombre"))) ?></td>
-        <td align="left" width="5%"><?php echo (utf8_decode($datoTmp->get_data("faeEstado"))) ?></td>
-		<td align="left" width="10%"><?php echo (utf8_decode($datoTmp->get_data("faeFechaInicio"))) ?></td>
-	    <td align="left" width="10%"><?php echo (utf8_decode($datoTmp->get_data("faeFechaTermino"))) ?></td>
-		<td align="left" width="20%"><?php echo (utf8_decode($datoTmp->get_data("faeResponsable"))) ?></td>
-	    <td align="left" width="10%"><?php echo (utf8_decode($datoTmp->get_data("faeTelefono"))) ?></td>
+		<td align="left" width="5%"><?php echo (utf8_decode($rs["id"])) ?></td>
+		<td align="left" width="15%"><?php echo (utf8_decode($rs["faeNombre"])) ?></td>
+        <td align="left" width="5%"><?php echo (utf8_decode($rs["faeEstado"])) ?></td>
+		<td align="left" width="10%"><?php echo (utf8_decode($rs["faeFechaInicio"])) ?></td>
+	    <td align="left" width="10%"><?php echo (utf8_decode($rs["faeFechaTermino"])) ?></td>
+		<td align="left" width="20%"><?php echo (utf8_decode($rs["faeResponsable"])) ?></td>
+	    <td align="left" width="10%"><?php echo (utf8_decode($rs["faeTelefono"])) ?></td>
 
 		<td align="left" width="15%">
-        <!--<img src="images/permisos.png" title="Permisos" class="oplistado" onclick="window.parent.configurarRegistro('<? echo($controller) ?>',<?php echo ($datoTmp->get_data("id")) ?>)" />-->
-        <img src="images/editar.gif" title="Editar" class="oplistado" onclick="window.parent.editarRegistro('<? echo($controller) ?>',<?php echo ($datoTmp->get_data("id")) ?>)" />
-        <img src="images/borrar.gif" title="Eliminar" class="oplistado" onclick="window.parent.abrirVentanaConfirmacion(<?php echo ($datoTmp->get_data("id")) ?>)" />
+        <!--<img src="images/permisos.png" title="Permisos" class="oplistado" onclick="window.parent.configurarRegistro('<? echo($controller) ?>',<?php echo ($rs["id"]) ?>)" />-->
+        <img src="images/editar.gif" title="Editar" class="oplistado" onclick="window.parent.editarRegistro('<? echo($controller) ?>',<?php echo ($rs["id"]) ?>)" />
+        <img src="images/borrar.gif" title="Eliminar" class="oplistado" onclick="window.parent.abrirVentanaConfirmacion(<?php echo ($rs["id"]) ?>)" />
         </td>
 	</tr>
-    <tr bgcolor="#FFFFFF" id="fila_sep_<?php echo ($datoTmp->get_data("id")) ?>">
-    	<td colspan="6" style="border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC; "></td>
+    <tr bgcolor="#FFFFFF" id="fila_sep_<?php echo ($rs["id"]) ?>">
+    	<td colspan="9" style="border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC; "></td>
 	</tr>
 	<?php
 	}
