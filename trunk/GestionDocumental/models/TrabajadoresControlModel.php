c7bef90f4686e22d78340a3034410f42f05f4720
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-11-07 23:25:30 +0000

    ajuste

--- trunk/GestionDocumental/models/TrabajadoresControlModel.php
+++ trunk/GestionDocumental/models/TrabajadoresControlModel.php
@@ -4,74 +4,79 @@ class TrabajadoresControlModel extends ModelBase
 	
 	public function bajaRegistro($array)
 	{
-		$dato = new Usuarios();
-		$dato->add_filter("id","=",$array["id"]);
-		$dato->load();
-		$dato->set_data("activo","N");
-		$dato->save();
-	}
+		$sql = " UPDATE trabajador SET activo = 'N' WHERE trbIdTrabajador = ".$param["id_reg"];
+		consulta($sql);
+	}		
 	
-	public function grabar_datosForm($array)
+	public function grabar_datosForm($param)
 	{
-		$tipop = $array["tipop"];
-		
-		$dato = new Trabajador();
-		if($tipop=="M")
+		$sql = " INSERT INTO trabajador SET ";
+		$sql_where = "";
+		if($param["tipop"]=="M")
 		{
-			$dato->add_filter("trbIdTrabajador","=",$array["id_usu"]);
-			$dato->load();
+			$sql = " UPDATE trabajador SET ";
+			$sql_where = " WHERE trbIdTrabajador = ".$param["id_reg"];
 		}
 		
-//autonumerico		if(trim($array["trbIdTrabajador"])<>"")$dato->set_data("trbIdTrabajador",$array["trbIdTrabajador"]);
-		if(trim($array["afpIdAfp"])<>"")$dato->set_data("afpIdAfp",$array["afpIdAfp"]);
-		if(trim($array["comIdComuna"])<>"")$dato->set_data("comIdComuna",$array["comIdComuna"]);
-		if(trim($array["ctrIdContratista"])<>"")$dato->set_data("ctrIdContratista",$array["ctrIdContratista"]);
-		if(trim($array["dirIdDireccion"])<>"")$dato->set_data("dirIdDireccion",$array["dirIdDireccion"]);
-		if(trim($array["isaIdIsapre"])<>"")$dato->set_data("isaIdIsapre",$array["isaIdIsapre"]);
-		if(trim($array["nacIdNacionalidad"])<>"")$dato->set_data("nacIdNacionalidad",$array["nacIdNacionalidad"]);
-		if(trim($array["tgrlIdCargoContractual"])<>"")$dato->set_data("tgrlIdCargoContractual",$array["tgrlIdCargoContractual"]);
-		if(trim($array["tgrlIdOficioCab"])<>"")$dato->set_data("tgrlIdOficioCab",$array["tgrlIdOficioCab"]);
-		if(trim($array["tgrlIdOficioDet"])<>"")$dato->set_data("tgrlIdOficioDet",$array["tgrlIdOficioDet"]);
-		if(trim($array["tgrlIdTipoContrato"])<>"")$dato->set_data("tgrlIdTipoContrato",$array["tgrlIdTipoContrato"]);
-		if(trim($array["tjorIdTipoJornada"])<>"")$dato->set_data("tjorIdTipoJornada",$array["tjorIdTipoJornada"]);
-		if(trim($array["trbAfectoArt22"])<>"")$dato->set_data("trbAfectoArt22",$array["trbAfectoArt22"]);
-		if(trim($array["trbAntiguedadMeses"])<>"")$dato->set_data("trbAntiguedadMeses",$array["trbAntiguedadMeses"]);
-		if(trim($array["trbApMaterno"])<>"")$dato->set_data("trbApMaterno",$array["trbApMaterno"]);
-		if(trim($array["trbApPaterno"])<>"")$dato->set_data("trbApPaterno",$array["trbApPaterno"]);
-		if(trim($array["trbCeco"])<>"")$dato->set_data("trbCeco",$array["trbCeco"]);
-		if(trim($array["trbDireccion"])<>"")$dato->set_data("trbDireccion",$array["trbDireccion"]);
-		if(trim($array["trbEstado"])<>"")$dato->set_data("trbEstado",$array["trbEstado"]);
-		if(trim($array["trbFechaContrato"])<>"")$dato->set_data("trbFechaContrato",$array["trbFechaContrato"]);
-		if(trim($array["trbFechaCreacion"])<>"")$dato->set_data("trbFechaCreacion",$array["trbFechaCreacion"]);
-		if(trim($array["trbFechaModificacion"])<>"")$dato->set_data("trbFechaModificacion",$array["trbFechaModificacion"]);
-		if(trim($array["trbFechaNac"])<>"")$dato->set_data("trbFechaNac",$array["trbFechaNac"]);
-		if(trim($array["trbHorasSemanales"])<>"")$dato->set_data("trbHorasSemanales",$array["trbHorasSemanales"]);
-		if(trim($array["trbIngresoObraFecha"])<>"")$dato->set_data("trbIngresoObraFecha",$array["trbIngresoObraFecha"]);
-		if(trim($array["trbNombre"])<>"")$dato->set_data("trbNombre",$array["trbNombre"]);
-		if(trim($array["trbPensionado"])<>"")$dato->set_data("trbPensionado",$array["trbPensionado"]);
-		if(trim($array["trbPerteneceSindicato"])<>"")$dato->set_data("trbPerteneceSindicato",$array["trbPerteneceSindicato"]);
-		if(trim($array["trbRut"])<>"")$dato->set_data("trbRut",$array["trbRut"]);
-		if(trim($array["trbRutJefe"])<>"")$dato->set_data("trbRutJefe",$array["trbRutJefe"]);
-		if(trim($array["trbSeguroCesantia"])<>"")$dato->set_data("trbSeguroCesantia",$array["trbSeguroCesantia"]);
-		if(trim($array["trbSexo"])<>"")$dato->set_data("trbSexo",$array["trbSexo"]);
-		if(trim($array["trbTelefono"])<>"")$dato->set_data("trbTelefono",$array["trbTelefono"]);
-		if(trim($array["trbTitulo"])<>"")$dato->set_data("trbTitulo",$array["trbTitulo"]);
-		if(trim($array["trbUsuarioCreacion"])<>"")$dato->set_data("trbUsuarioCreacion",$array["trbUsuarioCreacion"]);
-		if(trim($array["trbUsuarioModificacion"])<>"")$dato->set_data("trbUsuarioModificacion",$array["trbUsuarioModificacion"]);
-		if(trim($array["trbVisa"])<>"")$dato->set_data("trbVisa",$array["trbVisa"]);
-
-		$dato->set_data("activo","S");
-		$dato->save();
-	}
-
-	public function getUsuario($id_usuario)
-	{
-		$dato = new Usuarios();
-		$dato->add_filter("id_usuario","=",$id_usuario);
-		$dato->load();
+		if(trim($array["afpIdAfp"])<>"")$sql .= "afpIdAfp = '".$param["afpIdAfp"]."', ";
+		if(trim($param["comIdComuna"])<>"")$sql .= "comIdComuna = '".$param["comIdComuna"]."', ";
+		if(trim($param["ctrIdContratista"])<>"")$sql .= "ctrIdContratista = '".$param["ctrIdContratista"]."', ";
+		if(trim($param["dirIdDireccion"])<>"")$sql .= "dirIdDireccion = '".$param["dirIdDireccion"]."', ";
+		if(trim($param["isaIdIsapre"])<>"")$sql .= "isaIdIsapre = '".$param["isaIdIsapre"]."', ";
+		if(trim($param["nacIdNacionalidad"])<>"")$sql .= "nacIdNacionalidad = '".$param["nacIdNacionalidad"]."', ";
+		if(trim($param["tgrlIdCargoContractual"])<>"")$sql .= "tgrlIdCargoContractual = '".$param["tgrlIdCargoContractual"]."', ";
+		if(trim($param["tgrlIdOficioCab"])<>"")$sql .= "tgrlIdOficioCab = '".$param["tgrlIdOficioCab"]."', ";
+		if(trim($param["tgrlIdOficioDet"])<>"")$sql .= "tgrlIdOficioDet = '".$param["tgrlIdOficioDet"]."', ";
+		if(trim($param["tgrlIdTipoContrato"])<>"")$sql .= "tgrlIdTipoContrato = '".$param["tgrlIdTipoContrato"]."', ";
+		if(trim($param["tjorIdTipoJornada"])<>"")$sql .= "tjorIdTipoJornada = '".$param["tjorIdTipoJornada"]."', ";
+		if(trim($param["trbAfectoArt22"])<>"")$sql .= "trbAfectoArt22 = '".$param["trbAfectoArt22"]."', ";
+		if(trim($param["trbAntiguedadMeses"])<>"")$sql .= "trbAntiguedadMeses = '".$param["trbAntiguedadMeses"]."', ";
+		if(trim($param["trbApMaterno"])<>"")$sql .= "trbApMaterno = '".$param["trbApMaterno"]."', ";
+		if(trim($param["trbApPaterno"])<>"")$sql .= "trbApPaterno = '".$param["trbApPaterno"]."', ";
+		if(trim($param["trbCeco"])<>"")$sql .= "trbCeco = '".$param["trbCeco"]."', ";
+		if(trim($param["trbDireccion"])<>"")$sql .= "trbDireccion = '".$param["trbDireccion"]."', ";
+		if(trim($param["trbEstado"])<>"")$sql .= "trbEstado = '".$param["trbEstado"]."', ";
+		if(trim($param["trbFechaContrato"])<>"")$sql .= "trbFechaContrato = '".$param["trbFechaContrato"]."', ";
+		if(trim($param["trbFechaCreacion"])<>"")$sql .= "trbFechaCreacion = '".$param["trbFechaCreacion"]."', ";
+		if(trim($param["trbFechaNac"])<>"")$sql .= "trbFechaNac = '".$param["trbFechaNac"]."', ";
+		if(trim($param["trbHorasSemanales"])<>"")$sql .= "trbHorasSemanales = '".$param["trbHorasSemanales"]."', ";
+		if(trim($param["trbIngresoObraFecha"])<>"")$sql .= "trbIngresoObraFecha = '".$param["trbIngresoObraFecha"]."', ";
+		if(trim($param["trbNombre"])<>"")$sql .= "trbNombre = '".$param["trbNombre"]."', ";
+		if(trim($param["trbPensionado"])<>"")$sql .= "trbPensionado = '".$param["trbPensionado"]."', ";
+		if(trim($param["trbPerteneceSindicato"])<>"")$sql .= "trbPerteneceSindicato = '".$param["trbPerteneceSindicato"]."', ";
+		if(trim($param["trbRut"])<>"")$sql .= "trbRut = '".$param["trbRut"]."', ";
+		if(trim($param["trbRutJefe"])<>"")$sql .= "trbRutJefe = '".$param["trbRutJefe"]."', ";
+		if(trim($param["trbSeguroCesantia"])<>"")$sql .= "trbSeguroCesantia = '".$param["trbSeguroCesantia"]."', ";
+		if(trim($param["trbSexo"])<>"")$sql .= "trbSexo = '".$param["trbSexo"]."', ";
+		if(trim($param["trbTelefono"])<>"")$sql .= "trbTelefono = '".$param["trbTelefono"]."', ";
+		if(trim($param["trbTitulo"])<>"")$sql .= "trbTitulo = '".$param["trbTitulo"]."', ";
+		if(trim($param["trbVisa"])<>"")$sql .= "trbVisa = '".$param["trbVisa"]."', ";
+		$sql .= " activo = 'S'";
+		$sql .= $sql_where;
+		//echo("<br>SQL: ".$sql);
+		consulta($sql);
 		
-		return $dato;
+		if($param["tipop"] == "A")
+		{
+			$sql = " SELECT trbIdTrabajador FROM trabajador WHERE trbRut = '".$param["trbRut"]."' AND trbNombre = '".$param["trbNombre"]."' AND trbApPaterno = '".$param["trbApPaterno"]."' ORDER BY id DESC LIMIT 1" ;
+			$idsql_new = consulta($sql);
+			$rs_new = mysql_fetch_array($idsql_new);
+			$id_new = $rs_new["trbIdTrabajador"];
+			
+			$sql = " SELECT id FROM sub_tipodocumento ORDER BY id ASC ";
+			$idsql = consulta($sql);
+			while($rs=mysql_fetch_array($idsql))
+			{
+				$sql = " INSERT INTO documentotrabajador ";
+				$sql .= " (id_estado_documento, doctIdTrabajador, id_contratista, id_faena, id_sub_tipodocumento) ";
+				$sql .= " VALUES ";
+				$sql .= " (1, ".$id_new.", ".$param["ctrIdContratista"].", ".$param["id_faena"].",".$rs["id"].") ";
+			}
+		}
+		
+			//exit();
 	}
+
 	
 	public function getTrabajador($array)
 	{
@@ -85,20 +90,12 @@ class TrabajadoresControlModel extends ModelBase
 		return mysql_fetch_array($idsql);
 	}
 	
-	public function getDatosUsuario($array)
-	{
-		$dato = new Usuarios();
-		$dato->add_filter("id_usuario","=",$array["idusuario"]);
-		$dato->load();
-		
-		return $dato;
-	}
 		
 	public function getListaTrabajador($array)
 	{
 		include("config.php");
 		
-		$sql = " SELECT t.trbIdTrabajador trbIdTrabajador, t.trbNombre trbNombre, t.trbApPaterno trbApPaterno, t.trbRut trbRut ";
+		$sql = " SELECT trbIdTrabajador, trbNombre, trbApPaterno, trbRut ";
 		$sql .= " FROM trabajador t ";
 		$sql .= " WHERE t.activo = 'S' ";
 		
