9b6643c021cf035695ac594add0c064cc3edd527
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-11-07 23:14:59 +0000

    carga de archivos

--- trunk/GestionDocumental/js/dashboard.js
+++ trunk/GestionDocumental/js/dashboard.js
@@ -1,3 +1,33 @@
+function mostrar()
+{
+	var op = document.getElementById('controller').value;
+	var url = "index.php?controlador="+op+"&accion=listar_documentos";
+	var arrayInput = document.getElementsByTagName('input');
+	for(var i=0; i<arrayInput.length; i++)
+	{	 
+  		 if(arrayInput[i].getAttribute('filtro') == "S")
+   		 {
+			url += "&"+arrayInput[i].getAttribute('name')+"="+arrayInput[i].value;
+		 }
+	}
+	var arraySelect = document.getElementsByTagName('select');
+	for(var i=0; i<arraySelect.length; i++)
+	{	 
+  		 if(arraySelect[i].getAttribute('filtro') == "S")
+   		 {
+			url += "&"+arraySelect[i].getAttribute('name')+"="+arraySelect[i].value;
+		 }
+	}
+
+	$("#frmitemsadmin").load(url);
+}
+
+
+$(document).ready(function() 
+{
+	mostrar();
+});
+
 
 function verDocTrb(op,id)
 {
@@ -11,3 +41,41 @@ function abrirLiquidaciones(id)
 	$("#id_reg").val(id)
 }
 
+
+function abrirVentanaDoc(cont,vent,id)
+{
+//	alert(vent);
+	
+	$("#"+vent).css("margin-top",($(window).scrollTop()+50));
+	$("#"+vent).slideDown(1000);	
+	$("#id_reg").val(id)
+}
+
+
+function cargarTab(op,id)
+{
+	$("#id_tipodocumento").val(id);
+	//url = "index.php?controlador="+op+"&accion=listar_documentos&tipo_doc="+id;
+	//document.getElementById("frmsubpantalla").src = url;
+	mostrar();
+}
+
+function volverAdmin(op,id)
+{
+	var url = 'index.php?controlador='+op+'&accion=admin';
+	if(id > 0) url += "&id="+id; 
+	window.location = url;
+}
+
+function abrirVentanaDoc(id_trab, id_ts_doc)
+{
+	var idf = $("#id_faena").val();
+	//var id_ts_doc = $("#id_tipodocumento").val();
+	var id_contratista = $("#id_contratista").val();
+	var url = "index.php?controlador=DashBoard&accion=carga_documento";
+	url += "&id_d="+id_ts_doc;
+	url += "&id_t="+id_trab;
+	url += "&id_f="+idf;
+	url += "&id_c="+id_contratista;
+	window.location = url;
+}
