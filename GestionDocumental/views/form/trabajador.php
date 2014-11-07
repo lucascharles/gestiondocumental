a2be08e8bc216933411ac49a6e58e450b43df514
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-11-07 23:12:47 +0000

    edicion de trabajador

--- GestionDocumental/views/form/trabajador.php
+++ GestionDocumental/views/form/trabajador.php
@@ -2,9 +2,8 @@
 <? include("views/menu.php"); ?>
 <?
 	$titulo_form = "Alta Trabajador";
-	$rs = $dato;
+	
 	$id = ($tipop=="M") ? $rs["trbIdTrabajador"] : "";
-	$id_usuario = ($tipop=="M") ? $rs["trbIdTrabajador"] : "";
 	$ape_usuario = ($tipop=="M") ? $rs["trbApPaterno"] : "";
 	$nom_usu = ($tipop=="M") ? $rs["trbNombre"] : "";
 	$clave = ($tipop=="M") ? $rs["clave"] : "";
@@ -28,23 +27,26 @@
 	$trbFechaNac= ($tipop=="M") ? $rs["trbFechaNac"] : "";
 	$trbTelefono= ($tipop=="M") ? $rs["trbTelefono"] : "";
 	$trbTitulo= ($tipop=="M") ? $rs["trbTitulo"] : "";
+	$ctrIdContratista= ($tipop=="M") ? $rs["ctrIdContratista"] : "";
+	$id_faena= ($tipop=="M") ? $rs["id_faena"] : "";
+	
 
 	
 	if($tipop=="M")
 	{
-		$titulo_form = "Edici&oacute;n Trabajador(Id: ".$id_usuario.")";
+		$titulo_form = "Edici&oacute;n Trabajador (Id: ".$id.")";
 	}
 ?>
-<form name="frmTrabajadoresControl" action="" method='post'>
-<input type="hidden" name="id_usu" id="id_usu" value="<? echo($id)?>" />
+<form name="frm<?=$controller?>" action="" method='post'>
+<input type="hidden" name="id_reg" id="id_reg" value="<?=$id?>" />
 <input type="hidden" name="tipop" id="tipop" value="<? echo($tipop)?>" />
 
-<table  align="center" border="0" width="" cellpadding="0" cellspacing="0" id="formulario">
+<table  align="center" border="0" width="50%" cellpadding="0" cellspacing="0" id="formulario">
 	<tr>
 		<th align="left" colspan="3"><? echo($titulo_form) ?></th>
     </tr>
     <tr>
-    	<td align="right" class="etiqueta_form">Nombre:</td>
+    	<td align="right" class="etiqueta_form" width="150">Nombre:</td>
         <td align="left" class="etiqueta_form" colspan="1">
         <input type="text" name="trbNombre" id="trbNombre" value="<? echo(utf8_decode($nom_usu)); ?>" valida="requerido" tipovalida="texto" class="input_form_largo" onFocus="resaltar(this)" onBlur="noresaltar(this);" tabindex="0"/>
         </td>
@@ -62,19 +64,90 @@
         </td>
     </tr>
     <tr>
-    	<td align="right" class="etiqueta_form">ID:</td>
-        <td align="left" class="etiqueta_form" colspan="1">
-        <input type="text" name="trbIdTrabajador" id="trbIdTrabajador" value="<? echo($id_usuario); ?>" valida="requerido" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
-        </td>
-        <td align="left" class="etiqueta_form" >
-       
-        </td>
+     	<td align="right" class="etiqueta_form">Ap. Materno:</td>
+     	   <td align="left" class="etiqueta_form" colspan="1">
+     	   <input type="text" name="trbApMaterno" id="trbApMaterno" value="<? echo($trbApMaterno); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+     	   </td>
+     	   <td align="left" class="etiqueta_form" >
+     	  
+     	   </td>
+        </tr>    	
+     	
+        <tr>
+     	<td align="right" class="etiqueta_form">Ap Paterno:</td>
+     	   <td align="left" class="etiqueta_form" colspan="1">
+     	   <input type="text" name="trbApPaterno" id="trbApPaterno" value="<? echo($trbApPaterno); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+     	   </td>
+     	   <td align="left" class="etiqueta_form" >
+     	  
+     	   </td>
+        </tr>    	
+        <tr>
+     	<td align="right" class="etiqueta_form">Fecha Nac.:</td>
+     	   <td align="left" class="etiqueta_form" colspan="1">
+     	   <input type="text" name="trbFechaNac" id="trbFechaNac" value="<? echo($trbFechaNac); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+     	   </td>
+     	   <td align="left" class="etiqueta_form" >
+     	  
+     	   </td>
+        </tr>    	
+		<tr>
+    	<td align="right" class="etiqueta_form">Nacionalidad:</td>
+    	   <td align="left" class="etiqueta_form" colspan="1">
+    	   <input type="text" name="nacIdNacionalidad" id="nacIdNacionalidad" value="<? echo($nacIdNacionalidad); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+    	   </td>
+    	   <td align="left" class="etiqueta_form" >
+    	  
+    	   </td>
+    </tr>    	
+        <tr>
+     	<td align="right" class="etiqueta_form">Rut:</td>
+     	   <td align="left" class="etiqueta_form" colspan="1">
+     	   <input type="text" name="trbRut" id="trbRut" value="<? echo($trbRut); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+     	   </td>
+     	   <td align="left" class="etiqueta_form" >
+     	  
+     	   </td>
+        </tr>    	
+
+        <tr>
+     	<td align="right" class="etiqueta_form">Telefono:</td>
+     	   <td align="left" class="etiqueta_form" colspan="1">
+     	   <input type="text" name="trbTelefono" id="trbTelefono" value="<? echo($trbTelefono); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+     	   </td>
+     	   <td align="left" class="etiqueta_form" >
+     	  
+     	   </td>
+        </tr>    	
+
+        <tr>
+     	<td align="right" class="etiqueta_form">Sexo:</td>
+     	   <td align="left" class="etiqueta_form" colspan="1">
+     	   <input type="text" name="trbSexo" id="trbSexo" value="<? echo($trbSexo); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+     	   </td>
+     	   <td align="left" class="etiqueta_form" >
+     	  
+     	   </td>
+        </tr>    	
+
+        <tr>
+     	<td align="right" class="etiqueta_form">Titulo:</td>
+     	   <td align="left" class="etiqueta_form" colspan="1">
+     	   <input type="text" name="trbTitulo" id="trbTitulo" value="<? echo($trbTitulo); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+     	   </td>
+     	   <td align="left" class="etiqueta_form" >
+     	  
+     	   </td>
+        </tr>
+	<tr>
+	<td align="left" class="subtitulo_form" colspan="3">Direcci&oacute;n</td>
     </tr>
+    
 <!-- ------------------------------------------------------------------- -->
 	<tr>
 	<td align="right" class="etiqueta_form">Direccion:</td>
 	   <td align="left" class="etiqueta_form" colspan="1">
-	   <input type="text" name="dirIdDireccion" id="dirIdDireccion" value="<? echo($dirIdDireccion); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+	   <input type="text" name="dirIdDireccion" id="dirIdDireccion" value="<? echo($dirIdDireccion); ?>" valida="" tipovalida="texto" class="input_form_largo" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
 	   </td>
 	   <td align="left" class="etiqueta_form" >
 	  
@@ -84,14 +157,35 @@
 	<tr>
 	<td align="right" class="etiqueta_form">Comuna:</td>
 	   <td align="left" class="etiqueta_form" colspan="1">
-	   <input type="text" name="comIdComuna" id="comIdComuna" value="<? echo($comIdComuna); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+	   <input type="text" name="comIdComuna" id="comIdComuna" value="<? echo($comIdComuna); ?>" valida="" tipovalida="texto" class="input_form_largo" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
 	   </td>
 	   <td align="left" class="etiqueta_form" >
 	  
 	   </td>
    </tr>    	
-    	
-    	
+   <tr>
+	<td align="left" class="subtitulo_form" colspan="3">Contratista</td>
+    </tr> 	
+    <td align="right" class="etiqueta_form">Contratista:</td>
+	   <td align="left" class="etiqueta_form" colspan="1">
+	   <input type="text" name="ctrIdContratista" id="ctrIdContratista" value="<? echo($ctrIdContratista); ?>" valida="" tipovalida="texto" class="input_form_largo" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+	   </td>
+	   <td align="left" class="etiqueta_form" >
+	  
+	   </td>
+   </tr>    	
+   <td align="right" class="etiqueta_form">Faena:</td>
+	   <td align="left" class="etiqueta_form" colspan="1">
+	   <input type="text" name="id_faena" id="id_faena" value="<? echo($id_faena); ?>" valida="" tipovalida="texto" class="input_form_largo" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
+	   </td>
+	   <td align="left" class="etiqueta_form" >
+	  
+	   </td>
+   </tr>    	
+    
+   <tr>
+	<td align="left" class="subtitulo_form" colspan="3">Datos Laborales</td>
+    </tr>
    <tr>
 	<td align="right" class="etiqueta_form">Isapre:</td>
 	   <td align="left" class="etiqueta_form" colspan="1">
@@ -101,15 +195,7 @@
 	  
 	   </td>
    </tr>    	
-    	<tr>
-    	<td align="right" class="etiqueta_form">Nacionalidad:</td>
-    	   <td align="left" class="etiqueta_form" colspan="1">
-    	   <input type="text" name="nacIdNacionalidad" id="nacIdNacionalidad" value="<? echo($nacIdNacionalidad); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
-    	   </td>
-    	   <td align="left" class="etiqueta_form" >
-    	  
-    	   </td>
-       </tr>    	
+   
 
     	<tr>
     	<td align="right" class="etiqueta_form">Cargo Contractual:</td>
@@ -183,25 +269,7 @@
      	   </td>
         </tr>    	
      	
-        <tr>
-     	<td align="right" class="etiqueta_form">Ap. Materno:</td>
-     	   <td align="left" class="etiqueta_form" colspan="1">
-     	   <input type="text" name="trbApMaterno" id="trbApMaterno" value="<? echo($trbApMaterno); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
-     	   </td>
-     	   <td align="left" class="etiqueta_form" >
-     	  
-     	   </td>
-        </tr>    	
-     	
-        <tr>
-     	<td align="right" class="etiqueta_form">Ap Paterno:</td>
-     	   <td align="left" class="etiqueta_form" colspan="1">
-     	   <input type="text" name="trbApPaterno" id="trbApPaterno" value="<? echo($trbApPaterno); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
-     	   </td>
-     	   <td align="left" class="etiqueta_form" >
-     	  
-     	   </td>
-        </tr>    	
+        
         
      	<tr>
      	<td align="right" class="etiqueta_form">Ceco:</td>
@@ -213,55 +281,7 @@
      	   </td>
         </tr>    	
 
-        <tr>
-     	<td align="right" class="etiqueta_form">Fecha Nac.:</td>
-     	   <td align="left" class="etiqueta_form" colspan="1">
-     	   <input type="text" name="trbFechaNac" id="trbFechaNac" value="<? echo($trbFechaNac); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
-     	   </td>
-     	   <td align="left" class="etiqueta_form" >
-     	  
-     	   </td>
-        </tr>    	
-
-        <tr>
-     	<td align="right" class="etiqueta_form">Rut:</td>
-     	   <td align="left" class="etiqueta_form" colspan="1">
-     	   <input type="text" name="trbRut" id="trbRut" value="<? echo($trbRut); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
-     	   </td>
-     	   <td align="left" class="etiqueta_form" >
-     	  
-     	   </td>
-        </tr>    	
-
-        <tr>
-     	<td align="right" class="etiqueta_form">Telefono:</td>
-     	   <td align="left" class="etiqueta_form" colspan="1">
-     	   <input type="text" name="trbTelefono" id="trbTelefono" value="<? echo($trbTelefono); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
-     	   </td>
-     	   <td align="left" class="etiqueta_form" >
-     	  
-     	   </td>
-        </tr>    	
-
-        <tr>
-     	<td align="right" class="etiqueta_form">Sexo:</td>
-     	   <td align="left" class="etiqueta_form" colspan="1">
-     	   <input type="text" name="trbSexo" id="trbSexo" value="<? echo($trbSexo); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
-     	   </td>
-     	   <td align="left" class="etiqueta_form" >
-     	  
-     	   </td>
-        </tr>    	
-
-        <tr>
-     	<td align="right" class="etiqueta_form">Titulo:</td>
-     	   <td align="left" class="etiqueta_form" colspan="1">
-     	   <input type="text" name="trbTitulo" id="trbTitulo" value="<? echo($trbTitulo); ?>" valida="" tipovalida="texto" class="input_form_medio" onFocus="resaltar(this)" onBlur="noresaltar(this);" />
-     	   </td>
-     	   <td align="left" class="etiqueta_form" >
-     	  
-     	   </td>
-        </tr>    	
+            	
 
      	
 <!-- --------------------------------------------------------------- --> 
