a3ca2826171ab52f8222073f798e34eab2b9ce38
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-11-07 23:36:10 +0000

    carga documento

--- trunk/GestionDocumental/js/documento.js
+++ trunk/GestionDocumental/js/documento.js
@@ -1,19 +1,14 @@
-function cargarDocumento(id)
-{
-	
-	window.location = "index.php?controlador=Documento&accion=carga&id_tipo_documento="+id+"&id_faena="+$("#id_faena").val();
-	/*
-	alert(document.frmcargadoc.action);
-	document.frmcargadoc.submit();
-	*/
-}
 function grabarFormUploadArch(id)
 {
 	if(validar('N'))
 	{
 		if($.trim($("#archivo").val()) == "" || $.trim($("#doctNombreArchivo").val()) == "") return false;
-		setTimeout("document.frmcargadoc.action = 'index.php?controlador=Documento&accion=grabasubir_archivo&id_tipo_documeto="+id+"'",1);
+		setTimeout("document.frmcargadoc.action = 'index.php?controlador=DashBoard&accion=grabasubir_archivo'",1);
 		setTimeout("document.frmcargadoc.submit()",10);
 	}
 }
 
+function volverDocContratista(op,id)
+{
+	window.location = "index.php?controlador="+op+"&accion=verDetalle&id="+id;
+}
