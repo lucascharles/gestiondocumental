d2e53afedf5aac875a58999e1b3337d68e870c74
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-11-14 02:51:38 +0000

    cambio estado documento

--- GestionDocumental/views/person/carga_documento.php
+++ GestionDocumental/views/person/carga_documento.php
@@ -5,10 +5,10 @@ include("views/menu.php");
  ?>
 
 <form name="frmcargadoc" enctype='multipart/form-data' method='post' action='' target='_self'>
-<input type="hidden" name="id_faena" id="id_faena" value="<? echo($id_f)?>" />
+<input type="hidden" name="id_faena" id="id_faena" value="<?=$id_f?>" />
 <input type="hidden" name="id_trabajador" id="id_trabajador" value="<? echo($id_t)?>" />
 <input type="hidden" name="id_sub_tipodocumento" id="id_sub_tipodocumento" value="<? echo($id_d)?>" />
-<input type="hidden" name="id_contratista" id="id_contratista" value="<? echo($id_c)?>" />
+<input type="hidden" name="id_contratista" id="id_contratista" value="<?=$id_c?>" />
 
 <input type="hidden" name="tipop" id="tipop" value="<? echo($tipop)?>" />
 
@@ -63,9 +63,9 @@ include("views/menu.php");
             </tr>
             <tr>
             <td colspan="<?=$cont?>">
-            <table width="100%" cellpadding="0" cellspacing="0" align="center" border="0" bgcolor="#FFFFFF" id="listado_home">
+            <table width="100%" cellpadding="0" cellspacing="1" align="center" border="0" bgcolor="#ffffff" id="listado_home">
         		<tr>
-            	<th></th><th>Documento</th>
+            	<th colspan="5">Documentos</th>
             	</tr>
             	<?
 				if($idsql_doc > '')
@@ -73,9 +73,20 @@ include("views/menu.php");
                 while($rs=mysql_fetch_array($idsql_doc))
 				{
 				?>
-                <tr><td></td><td><?=$rs["doctNombreArchivo"]?></td></tr>
+                <tr bgcolor="#FFFFFF">
+                	<td></td>
+                    <td><?=$rs["doctNombreArchivo"]?></td>
+                    <td><?=$rs["estado_documento"]?></td>
+                    <td align="right"><a href="docvarios/<?=$id_c."_docs_".$id_f."/".$rs["doctNombreEncrip"]?>" target="_blank"><?=$rs["NombreOriginal"]?></a></td>
+                    <td align="center">
+                    <img id="<?=$rs["id_documento"]?>" src="images/editar_estado.png" style="cursor:pointer;" title="Cambiar Estado" onclick='editarEstado(this)' />&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img id="<?=$rs["id_documento"]?>" src="images/borrar.gif" style="cursor:pointer;" title="Borrar" onclick='borrarDocumento(this)' />
+                    </td>
+                    
+                </tr>
                 <tr bgcolor="#FFFFFF" >
-    			<td colspan="2" style="border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC; height:5px; "></td>
+    			<td colspan="5" style="border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC; height:5px; "></td>
+                
 				</tr>
                 <?
                 }
@@ -83,7 +94,7 @@ include("views/menu.php");
 				else
 				{
 				?>
-				<tr><td></td><td><? echo("Sin documentos")?></td></tr>
+				<tr><td colspan="5"><? echo("Sin documentos")?></td></tr>
                 <?
 				}
 				?>
