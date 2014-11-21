6a79584edec7a3af8e721499435f52eca3a1cb1c
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-11-21 00:55:16 +0000

    acceso empresas

--- GestionDocumental/views/empresa/faenas.php
+++ GestionDocumental/views/empresa/faenas.php
@@ -1,5 +1,5 @@
-<? 	include("views/cabecera.php"); 
- 	include("views/menu.php"); 
+<? 	include("views/cabecera_front.php"); 
+ 	include("views/menu_front.php"); 
  
  	$label = "Faenas";
 
@@ -14,7 +14,7 @@
 <input type="hidden" name="id_reg" id="id_reg" value="" />
 <input type="hidden" name="inicio" id="inicio" value="<? echo($ini) ?>" filtro="S" />
 <input type="hidden" name="inicio_pag" id="inicio_pag" value="<? echo($ini_pag) ?>" filtro="S" />
-<table  align="center" width="100%" border="0" cellpadding="0" cellspacing="0" id="formulario">
+<table  align="center" width="90%" border="0" cellpadding="0" cellspacing="0" id="formulario">
 	<tr>
 		<th align="left" colspan="3"><? echo($titulo_form) ?></th>
     </tr>
@@ -24,29 +24,18 @@
     
     <tr>
     	<td colspan="3">
-        	<table width="100%" border="0" cellpadding="0" cellspacing="3" align="center" id="buscador">
+        	<table width="100%" border="0" cellpadding="0" cellspacing="3" align="left" id="buscador">
 		        <tr>
 					<td colspan="1" align="right" width="50"  class="etiqueta_form">Nombre:</td>
-					<td colspan="1" align="left" width="200"  class="etiqueta_form"><input type="text" name="contratista" id="contratista"  filtro="S" value="<? echo($_SESSION["f_apellido"]) ?>"  class="input_form_largo" onFocus="resaltar(this)" onBlur="noresaltar(this)"  onkeyup='mostrar()' tabindex="0"/></td>
-
- 	 	 	 	 	<td colspan="1" align="right" width="50"  class="etiqueta_form">Estado:</td>
-					<td colspan="1" align="left" width="200"  class="etiqueta_form"><input type="text" name="apellido" id="apellido"  filtro="S" value="<? echo($_SESSION["f_apellido"]) ?>"  class="input_form_largo" onFocus="resaltar(this)" onBlur="noresaltar(this)"  onkeyup='mostrar()' tabindex="0"/></td>
-
- 	 	 	 	 	<td colspan="1" align="right" width="50"  class="etiqueta_form">Responsable:</td>
- 	 	 	 		<td colspan="1" align="left" width="200"  class="etiqueta_form"><input type="text" name="apellido" id="apellido"  filtro="S" value="<? echo($_SESSION["f_apellido"]) ?>"  class="input_form_largo" onFocus="resaltar(this)" onBlur="noresaltar(this)"  onkeyup='mostrar()' tabindex="0"/></td>
+					<td colspan="1" align="left" width=""  class="etiqueta_form"><input type="text" name="faeNombre" id="faeNombre"  filtro="S" value="<?=$_SESSION["f_faeNombre"] ?>"  class="input_form_largo" onFocus="resaltar(this)" onBlur="noresaltar(this)"  onkeyup='mostrar()' tabindex="0"/></td>
+               </tr>
+               <tr>
+	 	 	 	 	<td colspan="1" align="right" width="50"  class="etiqueta_form">Responsable:</td>
+ 	 	 	 		<td colspan="1" align="left" width=""  class="etiqueta_form"><input type="text" name="faeResponsable" id="faeResponsable"  filtro="S" value="<?=$_SESSION["f_faeResponsable"]?>"  class="input_form_largo" onFocus="resaltar(this)" onBlur="noresaltar(this)"  onkeyup='mostrar()'/></td>
  	 	 	 	</tr>
                 <tr>
-			     	<td align="right"  colspan="6" class="etiqueta_form">
-                    <table class="opciones" onclick="nuevoRegistro('<?=$controller?>')" title="Nueva Faena">
-                        <tr>
-                            <td align="right" valign="middle">
-                            <img src="images/nuevoregistro.png" onmouseover="resaltarImagen(this)" onmouseout="noresaltarImagen(this)"/>
-                            </td>
-                            <td align="left" valign="middle" >
-                            Nuevo 
-                            </td>
-                        </tr>
-                    </table>
+			     	<td align="right"  colspan="2" class="etiqueta_form">
+                    
                    </td>
 			    </tr>
              </table>
@@ -59,7 +48,7 @@
     </tr>  
 	<tr>
         <td colspan="3" valign="top">
-        	<div id="frmitemsadmin" style=" width:100%; min-height:1050px; overflow:auto;"></div>
+        	<div id="frmitemsadmin" style=" width:100%; min-height:1050px; overflow:auto; "></div>
          </td>
     </tr>  
 </table>
