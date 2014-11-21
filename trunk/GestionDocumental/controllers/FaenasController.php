571fd93cd68dbdcf414c169c27ea897dbaf542e5
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-11-21 01:03:23 +0000

    acceso empresas

--- trunk/GestionDocumental/controllers/FaenasController.php
+++ trunk/GestionDocumental/controllers/FaenasController.php
@@ -37,26 +37,22 @@ class FaenasController extends ControllerBase
 
 		require 'models/FaenasModel.php';
 		$dato = new FaenasModel();		
+		
 		$_SESSION["f_faeNombre"] = $param["faeNombre"];
-		$_SESSION["f_faeEstado"] = $param["faeEstado"];
+		$_SESSION["f_faeResponsable"] = $param["faeResponsable"];
 				
 		$data['controller'] = $param["controlador"];
-
-		$_SESSION["idusuario"] = $param["usrLogin"];
-		$_SESSION["userid"] = $resp[1]["id"];
-		
+				
+		$destino = "admin";	
 		if($_SESSION["tip_usuario"] == "E")
 		{
-			$idempresa = $_SESSION["idempresa"];
-			$data['result'] = $dato->getListaFaenasEmpresa($idempresa);
-			$this->view->show("empresa/lista_faenas.php", $data);
+			$destino = "empresa";
+			$param["id_empresa"] = $_SESSION["idempresa"];
 		}
-		else
-		{
 		
-			$data['result'] = $dato->getListaFaenas($param);
-			$this->view->show("admin/lista_faenas.php", $data);
-		}
+		$data['result'] = $dato->getListaFaenas($param);
+
+		$this->view->show($destino."/lista_faenas.php", $data);
 
 		
 	}
@@ -106,24 +102,23 @@ class FaenasController extends ControllerBase
 		$this->view->show("form/faena.php", $data);
 	}
 	
-	public function verDetalle($array)
+	public function verDetalle($param)
 	{
 		require 'models/FaenasModel.php';
 		require 'models/DocumentoModel.php';
 		require 'models/ContratistaModel.php';
 		
 		$faena = new FaenasModel();
-		$fae = $faena->getFaena($array);
-		$data['datoFaena'] = $fae;
-
 		$doc = new DocumentoModel();
+		
+		$data['datoFaena'] = $faena->getFaena($param);
 		$data['idsql_tip_doc'] = $doc->getListaTipoDocumento();
-		$data['rs_tip_doc'] = mysql_fetch_array($doc->getListaTipoDocumento());
+		$data['idsql_tip_doc_aux'] = $doc->getListaTipoDocumento();
 		$data['ctrIdContratista'] = $_SESSION["idempresa"];
-		$data['nom_sistema'] = $array["nombre_sistema"];
-		$data['controller'] = $array["controlador"];
+		$data['nom_sistema'] = $param["nombre_sistema"];
+		$data['controller'] = $param["controlador"];
 		$data['arrayscriptCss'] = array("smoothness/jquery-ui-1.8.17.custom.css");
-		$data['arrayscriptJs'] = array("dashboard.js","funciones.js");
+		$data['arrayscriptJs'] = array("faena_empresa.js","funciones.js");
 		
 		$this->view->show("empresa/detalle_faena.php", $data);
 	}
@@ -150,9 +145,65 @@ class FaenasController extends ControllerBase
 		$dato = new DocumentoModel();
 		$data['controller'] = $param["controlador"];
 		$data["idsql_doc_trabajador"] = $dato->getListaDocumentos($param);
-		$this->view->show("empresa/lista_dashboard_contatista.php", $data);
+		$this->view->show("empresa/lista_documento_empresa.php", $data);
 		
 	}
 	
+	public function carga_documento($param)
+	{
+		require 'models/ContratistaModel.php';
+		require 'models/DocumentoModel.php';
+		require 'models/FaenasModel.php';
+		require 'models/TrabajadoresControlModel.php';
+		$dato = new ContratistaModel();
+		$doc = new DocumentoModel();
+		$faena = new FaenasModel();
+		$trabajador = new TrabajadoresControlModel();
+		
+		$data["dato"]= $dato->getContratista(array("id"=>$param["id_c"]));
+		$data["rs_faena"]= $faena->getFaena(array("id"=>$param["id_f"]));
+		$data["rs_trabajador"]= $trabajador->getTrabajador(array("id"=>$param["id_t"]));
+		$data["rs_sub_doc"]= $doc->getSubTipoDocumento(array("id"=>$param["id_d"]));
+		$data['controller'] = $param["controlador"];
+		$data['nom_sistema'] = $param["nombre_sistema"];
+		$data['arrayscriptJs'] = array("validacampos.js","documento_empresa.js","funciones.js");
+		$data["id_f"] = $param["id_f"];
+		$data["id_t"] = $param["id_t"];
+		$data["id_d"] = $param["id_d"];
+		$data["id_c"] = $param["id_c"];
+		$data["id_td"] = $param["id_td"];
+		$data["idsql_doc"] = $doc->getDocumentos(array("id_sub_tipodocumento"=>$param["id_d"],"id_faena"=>$param["id_f"],"id_trabajador"=>$param["id_t"],"id_contratista"=>$param["id_c"]));
+		
+		$this->view->show("empresa/carga_documento.php", $data);
+	}
+	
+	public function grabasubir_archivo($param)
+	{
+		require 'models/ContratistaModel.php';
+		require 'models/DocumentoModel.php';
+		require 'models/FaenasModel.php';
+		require 'models/TrabajadoresControlModel.php';
+		$dato = new ContratistaModel();
+		$doc = new DocumentoModel();
+		$faena = new FaenasModel();
+		$trabajador = new TrabajadoresControlModel();
+		
+		$doc->uploadDocumento($param);
+		
+		$data["dato"]= $dato->getContratista(array("id"=>$param["id_contratista"]));
+		$data["rs_faena"]= $faena->getFaena(array("id"=>$param["id_faena"]));
+		$data["rs_trabajador"]= $trabajador->getTrabajador(array("id"=>$param["id_trabajador"]));
+		$data["rs_sub_doc"]= $doc->getSubTipoDocumento(array("id"=>$param["id_sub_tipodocumento"]));
+		$data['controller'] = $param["controlador"];
+		$data['nom_sistema'] = $param["nombre_sistema"];
+		$data['arrayscriptJs'] = array("validacampos.js","documento.js");
+		$data["id_f"] = $param["id_faena"];
+		$data["id_t"] = $param["id_trabajador"];
+		$data["id_d"] = $param["id_sub_tipodocumento"];
+		$data["id_c"] = $param["id_contratista"];
+		$data["idsql_doc"] = $doc->getDocumentos($param);
+		
+		$this->view->show("empresa/carga_documento.php", $data);
+	}
 }
 ?>
