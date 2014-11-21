832fd0869cfee7b57504d81e78635967820f23fa
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-11-21 00:54:24 +0000

    acceso empresas

--- trunk/GestionDocumental/views/empresa/lista_faenas.php
+++ trunk/GestionDocumental/views/empresa/lista_faenas.php
@@ -1,18 +1,16 @@
 <? 
-include("views/cabecera_listado.php"); 
+//include("views/cabecera_listado.php"); 
 ?>
 <table width="100%" cellpadding="0" cellspacing="0" align="center" border="0" bgcolor="#FFFFFF" id="listado">
 	
 	<tr>
-    	<th width="" align="center" height="25"></th>
 		<th align="left" width="">ID</th>
         <th align="left" width="">NOMBRE</th>
-        <th align="left" width="">ESTADO</th>
-		<th align="left" width="">FECHA INICIO</th>
-		<th align="left" width="">FECHA FIN</th>
+		<th align="left" width="">INICIO</th>
+		<th align="left" width="">FIN</th>
 		<th align="left" width="">RESPONSABLE</th>
 		<th align="left" width="">TELEFONO</th>
-        <th align="left" width=""></th>
+        <th align="left" width="">&nbsp;</th>
     </tr>
 	<?php
 	
@@ -20,22 +18,19 @@ include("views/cabecera_listado.php");
 	{		
 	?>
 	<tr bgcolor="#FFFFFF" id="fila_<?php echo ($rs["id"]) ?>">
-    	<td height="" width="5%"></td>
 		<td align="left" width="5%"><?php echo (utf8_decode($rs["id"])) ?></td>
-		<td align="left" width="15%"><?php echo (utf8_decode($rs["faeNombre"])) ?></td>
-        <td align="left" width="5%"><?php echo (utf8_decode($rs["faeEstado"])) ?></td>
-		<td align="left" width="10%"><?php echo (utf8_decode($rs["faeFechaInicio"])) ?></td>
-	    <td align="left" width="10%"><?php echo (utf8_decode($rs["faeFechaTermino"])) ?></td>
+		<td align="left" width="25%"><?php echo (utf8_decode($rs["faeNombre"])) ?></td>
+		<td align="left" width="10%"><?=utf8_decode(formatoFecha($rs["faeFechaInicio"],"yyyy-mm-dd","dd/mm/yyyy")) ?></td>
+	    <td align="left" width="10%"><?=utf8_decode(formatoFecha($rs["faeFechaTermino"],"yyyy-mm-dd","dd/mm/yyyy")) ?></td>
 		<td align="left" width="20%"><?php echo (utf8_decode($rs["faeResponsable"])) ?></td>
 	    <td align="left" width="10%"><?php echo (utf8_decode($rs["faeTelefono"])) ?></td>
 
-		<td align="left" width="15%">
-        <img src="images/editar.gif" title="Ver" class="oplistado" onclick="window.parent.verDetalles('<? echo($controller) ?>',<?php echo ($rs["id"]) ?>)" />
-        <img src="images/borrar.gif" title="Eliminar" class="oplistado" onclick="window.parent.abrirVentanaConfirmacion(<?php echo ($rs["id"]) ?>)" />
+		<td align="left" width="20%">
+        <img src="images/carpetas.gif" title="Documentos" class="oplistado" onclick="window.parent.verDetalles('<? echo($controller) ?>',<?php echo ($rs["id"]) ?>)" />
         </td>
 	</tr>
-    <tr bgcolor="#FFFFFF" id="fila_sep_<?php echo ($rs["id"]) ?>">
-    	<td colspan="9" style="border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC; "></td>
+    <tr bgcolor="#FFFFFF" id="fila_sep_<?=$rs["id"]?>">
+    	<td colspan="7" style="border-bottom:solid; border-bottom-width:2px; border-bottom-color:#CCCCCC; "><div style="height:1px;"></div></td>
 	</tr>
 	<?php
 	}
