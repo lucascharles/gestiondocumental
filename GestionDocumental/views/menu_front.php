d7eacea9a8eab00386f6a490e66d8eada6593836
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2014-11-21 00:57:51 +0000

    acceso empresa

---
+++ GestionDocumental/views/menu_front.php
@@ -0,0 +1,29 @@
+<table width="90%" align="center" border="0" cellpadding="0" cellspacing="0">
+   <tr>
+   		<td>
+        	<ul id="nav">
+            	<li>
+			<div class="principal" onClick="cargarOpcion('DashBoar@faenasempresa')" onMouseOver="resaltarOpMenu(this)" onMouseOut="noresaltarOpMenu(this)">DASHBOARD</div>
+            	</li>
+            </ul>
+		</td>
+    	<td>
+        	<ul id="nav">
+            	<li>
+			<div class="principal" onClick="cargarOpcion('TrabajadoresControl')" onMouseOver="resaltarOpMenu(this)" onMouseOut="noresaltarOpMenu(this)">TRABAJADORES</div>
+            </li>
+            </ul>
+		</td>
+        <td width="80%">
+        </td>
+	</tr>
+    <tr>
+	<td colspan="3">
+    	<hr class="separa_menu" />
+    </td>
+</tr>
+</table>
+
+
+
+
