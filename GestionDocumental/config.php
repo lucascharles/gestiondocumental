69d8ffb454711ebfdce3551b51c296fce2e655b1
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2015-01-15 18:02:34 +0000

    se agregan datos para envio de mail de contacto

--- GestionDocumental/config.php
+++ GestionDocumental/config.php
@@ -13,7 +13,9 @@ $config->set('dbname', 'gestion_documental');
 $config->set('dbuser', 'root');
 $config->set('dbpass', 'root');
 $config->set('carpeta_archivos_general', 'docvarios/');
-
+$config->set('destino_mail','seba.charles@gmail.com');
+$config->set('destino_nombre','Administracion');
+$config->set('mail_envio',"info@bc-soluciones.com");
 $_SESSION["config_obj"] = $config;
 $include_url = $_SERVER[DOCUMENT_ROOT]."/gestiondocumental";
 ?>
