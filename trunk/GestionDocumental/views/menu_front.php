c9a3b3b844d61280029a4df78b8b9740f74944b5
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2015-01-17 20:14:40 +0000

    ajustes Trabajador

--- trunk/GestionDocumental/views/menu_front.php
+++ trunk/GestionDocumental/views/menu_front.php
@@ -10,7 +10,7 @@
     	<td>
         	<ul id="nav">
             	<li>
-			<div class="principal" onClick="cargarOpcion('TrabajadoresControl')" onMouseOver="resaltarOpMenu(this)" onMouseOut="noresaltarOpMenu(this)">TRABAJADORES</div>
+			<div class="principal" onClick="cargarOpcion('Trabajador')" onMouseOver="resaltarOpMenu(this)" onMouseOut="noresaltarOpMenu(this)">TRABAJADORES</div>
             </li>
             </ul>
 		</td>
