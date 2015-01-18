1bc5fbd5741c8f8b227fd958d8ff431ca95b4a62
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2015-01-18 12:56:28 +0000

    se agrega en la sesion el array para el combo si/no

--- GestionDocumental/init.php
+++ GestionDocumental/init.php
@@ -1,3 +1,10 @@
 <?
 $_SESSION["tablas_audi"] = array();
+
+$combo = array();
+$combo[0][0] = 1;
+$combo[0][1] = "SI";
+$combo[1][0] = 2;
+$combo[1][1] = "NO";
+$_SESSION["combo_sino"] = $combo;
 ?>
