317710f0bb5e35ee6424d10dd902d2a770db9018
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2015-01-18 13:03:24 +0000

    parametros para sql

--- GestionDocumental/models/CiudadesModel.php
+++ GestionDocumental/models/CiudadesModel.php
@@ -25,15 +25,49 @@ class CiudadesModel extends ModelBase
 		include("config.php");
 		
 		$sql .= " SELECT * ";
-		$sql .= " FROM ciudades c";
+		$sql .= " FROM ciudades c ";
 		$sql .= " WHERE c.activo = 'S' ";
 		$sql .= " ORDER BY c.ciudad ";
+
+		if($param["idRegion"] > 0)
+		{
+			$sql .= " AND c.idRegion = ".$param["idRegion"];
+		}
 
 		$result = consulta($sql);
 		
     	return $result;	
 	}
 	
+	public function get_combo_ciudad($param)
+	{
+		include("config.php");
+		
+		$sql = " SELECT c.idCiudad 	, c.ciudad ";
+		$sql .= " FROM ciudades c ";
+		$sql .= " WHERE c.activo = 'S' ";
+		
+	
+		if(trim($param["idRegion"]) <> "")
+		{
+			$sql .= " and c.idRegion = ".$param["idRegion"];
+		}
+		
+		$sql .= " ORDER BY c.ciudad ";
+		
+// 		echo($sql);
+// 		exit;
+		
+		$idsql = consulta($sql);
+		$html_combo = "";
+		$html_combo .= "<option value=''></option> ";
+		while($rs=mysql_fetch_array($idsql))
+		{
+			$html_combo .= "<option value='".$rs["idCiudad"]."'>".utf8_encode($rs["ciudad"])." </option> ";
+		}
+				
+    	return $html_combo;	
+	}
 	
 }
 ?>
