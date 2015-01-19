41ab1ea40aff0f6c168d093e970c287a48c79e07
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2015-01-19 02:48:03 +0000

    nuevo

---
+++ trunk/GestionDocumental/views/help.php
@@ -0,0 +1,8 @@
+<table  border="0" cellpadding="0" cellspacing="0" align="right" id="leyendas" style="background-color:#FFFFFF; border:solid; border-width:1px; border-radius:3px; border-color:#C0DECF; margin-right:13px; -moz-box-shadow: 1px 1px 3px #ccc; -webkit-box-shadow: 1px 1px 3px #ccc; box-shadow: 1px 1px 3px #ccc;	/* IE 8 */ -ms-filter: "progid:DXImageTransform.Microsoft.Shadow(Strength=4, Direction=135, Color='#cccccc')";	filter: progid:DXImageTransform.Microsoft.Shadow(Strength=4, Direction=135, Color='#cccccc');">
+         		<tr>
+            		<td align="right"><img src="images/alerta.png"  /></td><td align="left"> <font style="color:#0000CC; font-weight:600; font-size:11px;">SIN SUBIR</font></td><td width="7"></td>
+                	<td align="right"><img src="images/advertencia.png"  /></td><td align="left"><font style="color:#0000CC; font-weight:600; font-size:11px;">SIN REVISAR</font></td><td width="7"></td>
+                	<td align="right"><img src="images/cancelar.gif"  /></td><td align="left"><font style="color:#0000CC; font-weight:600; font-size:11px;">SIN REVISAR</font></td><td width="7"></td>
+                	<td align="right"><img src="images/activar.gif"  /></td><td align="left"><font style="color:#0000CC; font-weight:600; font-size:11px;">SIN REVISAR</font></td><td width="5"></td>
+            	</tr>
+        	</table>
