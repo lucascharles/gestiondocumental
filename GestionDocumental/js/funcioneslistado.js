8ec7c1f970a6d8b5bc92f8e57d589e30a0ce8ef5
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2015-02-05 12:05:12 +0000

    metodos nuevos

--- GestionDocumental/js/funcioneslistado.js
+++ GestionDocumental/js/funcioneslistado.js
@@ -1,4 +1,56 @@
 function mostrarListado(form)
 {
+	document.frmlistado.filtrar.value = 1;	
 	document.frmlistado.submit();
+}
+
+function imprimirHtml()
+{
+	window.print();
+}
+
+function imprimirExcel()
+{
+	window.open("views/person/exportar_a_excel.php");
+}
+
+function imprimirPdf()
+{
+	window.open("index.php?controlador=Informes&accion=get_trabajdores_pdf");
+}
+
+function habilitarDescarga(obj)
+{
+	if($.trim($(obj).val()) != "")
+	{
+		$("#content_descarga").show("slow");
+	}
+	else
+	{
+		$("#content_descarga").hide("slow");
+	}
+}
+
+function descargarDocumentos()
+{
+	
+	var datos = "controlador=Informes&accion=bajar_trabajdores_pdf&id="+$("#id_tipodocumento").val();
+	datos += "&lotes="+$("#lotes").val();
+	
+				$.ajax({
+					url: "index.php",
+					type: "GET",
+					data: datos,
+					cache: false,
+					success: function(res)
+					{
+						window.open("index.php?controlador=Informes&accion=mostrar_descarga");
+					},
+					error: function()
+					{
+						//$("#mensaje").text("Ha ocurrido un error y no se ha podido agregar el registro.");
+						//setTimeout("$('#mensaje').text('')",3000);
+					}
+				});
+	
 }
