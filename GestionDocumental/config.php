12ffaf13e9a6d2d7c54fccb03e675186a230dd32
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2015-02-05 11:33:47 +0000

    Se quita valores de acceso a la base del objeto config, para que no esten en la session, por seguridad

--- GestionDocumental/config.php
+++ GestionDocumental/config.php
@@ -8,14 +8,16 @@ $config->set('nombre_sistema','GESTION DOCUMENTAL');
 $config->set('nombre_empresa','BCH-SOLUCIONES S.A.');
 
 // CONFIGURACION MYSQL
-$config->set('dbhost', 'localhost');
-$config->set('dbname', 'gestion_documental');
-$config->set('dbuser', 'root');
-$config->set('dbpass', 'root');
 $config->set('carpeta_archivos_general', 'docvarios/');
 $config->set('destino_mail','seba.charles@gmail.com');
 $config->set('destino_nombre','Administracion');
 $config->set('mail_envio',"info@bc-soluciones.com");
+
+$dbhost = 'localhost';
+$dbname = 'gestion_documental';
+$dbuser = 'root';
+$dbpass = 'root';
+
 $_SESSION["config_obj"] = $config;
 $include_url = $_SERVER[DOCUMENT_ROOT]."/gestiondocumental";
 ?>
