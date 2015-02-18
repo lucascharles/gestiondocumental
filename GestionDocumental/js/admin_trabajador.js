5477ca2685904a205919e73142c8bdf31e3739b9
seba.charles@gmail.com <seba.charles@gmail.com@71596901-417f-03a1-804a-d3748f7621a6>
2015-02-18 12:00:58 +0000

    bloqueo por agencia

--- GestionDocumental/js/admin_trabajador.js
+++ GestionDocumental/js/admin_trabajador.js
@@ -3,3 +3,9 @@ function documentosTrabajador(op,id,id_c)
 {
 	window.location = 'index.php?controlador='+op+'&accion=documentos_trabajador'+'&id='+id+'&id_c='+id_c;
 }
+
+function bloquear(id,bloq)
+{
+	if(bloq==1)	$("#"+id).hide();
+	if(bloq==0)	$("#"+id).show();
+}
